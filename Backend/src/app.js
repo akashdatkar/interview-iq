6ce8e9d7d47e100ed29db6cbee3bc21dffcf4474
akashdatkar <apdatkar2004@gmail.com>
2026-07-17 21:57:04 +0530

    Update Backend Env

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,7 +11,7 @@ const app=express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: process.env.FRONTEND_URL, // Replace with your frontend URL
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173', // Replace with your frontend URL
     credentials: true, // Allow cookies to be sent
 }));
 
